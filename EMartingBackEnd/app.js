b48c976a2afa6e27f9314e86e6f1981dc6580f10
DeepeshDragoneel <deepeshash444@gmail.com>
2021-08-10 11:48:04 +0530

    Made it better

--- EMartingBackEnd/app.js
+++ EMartingBackEnd/app.js
@@ -20,8 +20,8 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded( {extended: true} ))
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(express.json({ limit: "50mb" }))
-app.use(express.urlencoded({limit: "50mb", extended: true}))
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
 app.use("/uploads", express.static("./uploads"));
 app.use("/UserAuth",(req, res, next) => {
