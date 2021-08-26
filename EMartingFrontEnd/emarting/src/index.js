27c4e69487de5f580253b0e82ced33fa5c7767fd
DeepeshDragoneel <deepeshash444@gmail.com>
2021-08-26 11:15:17 +0530

    Added hash router

--- EMartingFrontEnd/emarting/src/index.js
+++ EMartingFrontEnd/emarting/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
